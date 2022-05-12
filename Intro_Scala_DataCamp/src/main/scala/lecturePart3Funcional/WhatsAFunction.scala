eeb623e4c4f179adf7002aeff897849dbda49f81
Corderodedios182 <corderodedios182@gmail.com>
2022-05-11 20:16:52 -0500

    Programación Funcional : Introducción Scala

---
+++ Intro_Scala_DataCamp/src/main/scala/lecturePart3Funcional/WhatsAFunction.scala
@@ -0,0 +1,13 @@
+package lecturePart3Funcional
+
+object WhatsAFunction {
+
+  //Funciones como elementos de primera clase : Trabajar con funciones como trabajamos con valores simples.
+
+  def Hola(palabra: String):String = {
+    println("Hola")
+  }
+
+
+
+}
