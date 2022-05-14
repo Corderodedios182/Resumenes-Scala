5c12a97c0f97ce6058169655186ee4029b284d8d
Corderodedios182 <corderodedios182@gmail.com>
2022-05-14 08:14:52 -0500

    Programación Funcional : Funciones de tipo = Function[A,B]

--- Intro_Scala_DataCamp/src/main/scala/lecturePart3Funcional/WhatsAFunction.scala
+++ Intro_Scala_DataCamp/src/main/scala/lecturePart3Funcional/WhatsAFunction.scala
@@ -8,6 +8,17 @@ object WhatsAFunction extends App {
 
   println(doubler(2))
 
+  // funciones de tipo = Function[A,B]
+  val strintToIntConverter = new Function[String, Int] {
+    override def apply(string: String): Int = string.toInt
+  }
+
+  println(strintToIntConverter("1000" + 145))
+
+  val adder: ((Int,Int) => Int) = new Function2[Int, Int, Int] {
+    override def apply(a: Int, b:Int): Int = a + b
+  }
+
 }
 
 trait MyFunction[A,B] {
