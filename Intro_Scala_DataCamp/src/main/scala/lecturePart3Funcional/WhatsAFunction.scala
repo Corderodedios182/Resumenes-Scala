8e184ce6c6b4da3b62cb99632a3c838ab6d9cf9f
Corderodedios182 <corderodedios182@gmail.com>
2022-05-14 08:45:19 -0500

    Programación Funcional : Funciones de tipo Function2[A,B,R] === (A,B) => R

--- Intro_Scala_DataCamp/src/main/scala/lecturePart3Funcional/WhatsAFunction.scala
+++ Intro_Scala_DataCamp/src/main/scala/lecturePart3Funcional/WhatsAFunction.scala
@@ -1,5 +1,7 @@
 package lecturePart3Funcional
 
+//Queremos poder pasar funciones como parámetros o devolver funciones como resultados.
+
 object WhatsAFunction extends App {
 
   val doubler = new MyFunction[Int, Int] {
@@ -18,6 +20,26 @@ object WhatsAFunction extends App {
   val adder: ((Int,Int) => Int) = new Function2[Int, Int, Int] {
     override def apply(a: Int, b:Int): Int = a + b
   }
+
+  //Funciones de tipo Function2[A,B,R] === (A,B) => R
+
+  def concatenator: (String, String) => String = new Function2[String,String,String] {
+    override def apply(a: String, b: String): String = a + " - " +  b
+  }
+
+  println(concatenator("Carlo","Flores"))
+
+  //Función que devuelve una función.
+  val supperAdder: Function1[Int, Function1[Int, Int]] = new Function[Int, Function1[Int, Int]] {
+    override def apply(x: Int): Function1[Int, Int] = new Function[Int, Int] {
+      override def apply(y: Int): Int = x + y
+    }
+  }
+
+
+  val adder3 = supperAdder(3)
+  println(adder3(4))
+  println(supperAdder(3)(4)) //funciones curried, tienen la propiedad que pueden llamarse con múltiples listas de parámetros simplemente por su mera definición.
 
 }
 
