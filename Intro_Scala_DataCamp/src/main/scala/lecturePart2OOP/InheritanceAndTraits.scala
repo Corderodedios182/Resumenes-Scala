0d68bc90ca2cb162acb02d2b01ebd7e779ec4766
Corderodedios182 <corderodedios182@gmail.com>
2022-05-16 20:02:31 -0500

    OOP : Introducción a Herencia

--- Intro_Scala_DataCamp/src/main/scala/lecturePart2OOP/InheritanceAndTraits.scala
+++ Intro_Scala_DataCamp/src/main/scala/lecturePart2OOP/InheritanceAndTraits.scala
@@ -4,29 +4,50 @@ object InheritanceAndTraits extends App {
 
   // Herencia clase simple
   class Animal {
-    protected def eat = println("nommon")
+    val creatureType = "wild"
+    def eat = println("crunch") //Método público para heredar.
+
+    protected def fly: Unit = println("Vuela") //Metodo privado no disponible en otras clases.
   }
 
+  //Se heredan los métodos de la clase animal.
   class Cat extends Animal {
     def crunch = {
-      eat
-      println("crunch crunch")
+      //métodos eat y fly se heredan de Animal, uno es público lo que implica se pueden llamar fuera de la clase y
+      //el otro método es privado solo puede heredarse y usarse dentro de la subclase.
+      println(s" $eat , 1 . crunch crunch")
+      fly //Se puede usar dentro de las Clases pero no fuera de está.
     }
   }
 
   val cat = new Cat
 
-  // Constructor
+  println(cat.crunch) //Métods públicos
+  println(cat.eat)
+  //cat.fly método //Método privado, fly bloqueado con proteced no es accesible fuera de la subclase
+
+
+  // Constructor actúan de una manera muy particular cuando se trata de herencia.
   class Person(name: String, age: Int) {
-    def this(name: String) = this(name, 0)
+    def this(name: String) = this(name, 0) //Nos permite instanciar la clase sin algun parametro
   }
 
-  class Adult(name: String, age: Int, idCard: String) extends Person(name, age)
+  class Adult(name: String, age: Int, idCard: String) extends Person(name)
 
-  //Overriding
-  //Class Dog (Override val creatureType: Strin) extends Animal {
-  //    override val creatureType = "domestic
-  //  override def eat = println("crunch, crunch")
-  // }
+  //Overriding : Anulando métodos de una clase.
+
+  class Dog(dogType: String) extends Animal {
+    override val creatureType = dogType //Anulamos atributo heredado de la clase animal.
+    override def eat = println("CRUNCH CRUNCH") //Anulamos el método heredado eat de la clase animal.
+  }
+  val dog = new Dog("K0")
+  dog.eat
+  println(dog.creatureType)
+
+  //Sustitución de Tipo: Polymorfismo.
+  val unknowAnimal: Animal = new Dog("K9")
+  unknowAnimal.eat
+
+  // Overriding vs OberLoading
 
 }
