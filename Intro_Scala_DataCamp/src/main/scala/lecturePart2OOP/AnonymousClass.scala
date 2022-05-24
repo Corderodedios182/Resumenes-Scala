df3b111344dbdf8fb6498eba8d70a5311290ab76
Corderodedios182 <corderodedios182@gmail.com>
2022-05-24 18:49:55 -0500

    OPP : Anoymous Class

--- Intro_Scala_DataCamp/src/main/scala/lecturePart2OOP/AnonymousClass.scala
+++ Intro_Scala_DataCamp/src/main/scala/lecturePart2OOP/AnonymousClass.scala
@@ -1,5 +1,32 @@
 package lecturePart2OOP
 
-object AnonymousClass {
+object AnonymousClass extends App {
+
+  abstract class Animal {
+    def eat: Unit
+  }
+
+  //anonymous class
+  val funnyAnimal: Animal = new Animal {
+    override def eat: Unit = println("ahahahahaha")
+  }
+  /*
+  equivale a
+
+  class AnonymousClasses$$anon$l extends Animal {
+    override def eat: Unit = println("aahahahahaha")
+    }
+  val funnyAnimal: Animal = new AnonymousClasses$$anon$1
+   */
+
+  println(funnyAnimal.getClass)
+
+  class Person(name: String){
+    def sayHi: Unit = println(s"Hi $name")
+  }
+
+  val jim = new Person("Jim") {
+    override def sayHi: Unit = println(s"Hi Jim")
+  }
 
 }
